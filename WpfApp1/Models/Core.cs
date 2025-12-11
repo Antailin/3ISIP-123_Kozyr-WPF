18903c4c348108a4194e6075dd57d70e4250370f
Антон Козырь <antail.for.game@gmail.com>
2025-12-11 11:11:39 +0300

    Добавил классы модели машины, двигателя, выбранных опций и хранилище данных

---
+++ WpfApp1/Models/Core.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class Core
+    {
+    }
+    public class CarModel
+    {
+        public string Name { get; set; }
+        public decimal BasePrice { get; set; }
+        public override string ToString() => $"{Name} - ({BasePrice} руб.)";
+    }
+
+    public class Engine
+    {
+        public string Type { get; set; } 
+        public decimal PriceModifier { get; set; }
+        public override string ToString() => $"{Type} (+{PriceModifier} руб.)";
+    }
+
+    public class CarOption
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public bool IsSelected { get; set; }
+    }
+    public static class AppData
+    {
+        public static Core CurrentConfig { get; set; } = new Core();
+    }
+}
