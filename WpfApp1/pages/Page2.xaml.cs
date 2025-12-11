b47c1abd64e2cd7e5b85e3586eaddbab3cb5c41e
Антон Козырь <antail.for.game@gmail.com>
2025-12-11 10:58:22 +0300

    Создал главную страницу, добавил пока что пустые отдельные страницы, так же написал код, для того, чтобы программа загружала первую страницу при старте

--- WpfApp1/pages/Page2.xaml.cs
+++ WpfApp1/pages/Page2.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,18 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WpfApp1.pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Page2.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Page2 : Page
     {
-        public MainWindow()
+        public Page2()
         {
             InitializeComponent();
-            MainFrame.Navigate(new pages.Page1());
-            StepProgressBar.Value = 1;
         }
     }
 }
