7fa94e2cc31343586516eb15c587bbad97947467
Антон Козырь <antail.for.game@gmail.com>
2026-03-11 10:55:37 +0300

    сделал абстрактный класс предметов (для оружия и брони)

--- WpfApp1/Models/Item.cs
+++ WpfApp1/Models/Item.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace WpfApp1.Models
 {
-    internal class Item
+    public abstract class Item
     {
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public string ImagePath { get; protected set; }
+
+        protected Item(string name, string description, string imagePath)
+        {
+            Name = name;
+            Description = description;
+            ImagePath = imagePath;
+        }
     }
 }
