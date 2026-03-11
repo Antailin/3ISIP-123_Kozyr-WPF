2d51f73de6d56eb23f33d52a568de3c39dcb21d1
Антон Козырь <antail.for.game@gmail.com>
2026-03-11 11:06:20 +0300

    сделал класс брони

--- WpfApp1/Models/Armor.cs
+++ WpfApp1/Models/Armor.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace WpfApp1.Models
 {
-    internal class Armor
+    public class Armor : Item
     {
+        public int DefenseBonus { get; }
+
+        public Armor(string name, int defenseBonus, string imagePath) : base(name, $"бонус к защите - {defenseBonus}", imagePath)
+        {
+            DefenseBonus = defenseBonus;
+        }
     }
 }
