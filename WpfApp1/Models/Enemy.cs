ee11ed2e9c5c48094588ab19ca29ef1c382f8f7d
Антон Козырь <antail.for.game@gmail.com>
2026-03-11 11:34:11 +0300

    сделал абстрактный класс для врагов

--- WpfApp1/Models/Enemy.cs
+++ WpfApp1/Models/Enemy.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfApp1.Models
 {
-    internal class Enemy
+    public abstract class Enemy
     {
+        public string Name { get; protected set; }
+        public int HP { get; protected set; }
+        public int AttackDMG { get; protected set; }
+        public int Defense { get; protected set; }
+        public string ImagePath { get; protected set; }
+
+        public bool IsAlive => HP > 0;
+
+        protected Enemy(string name, int hP, int attackDMG, int defense, string imagePath)
+        {
+            Name = name;
+            HP = hP;
+            AttackDMG = attackDMG;
+            Defense = defense;
+            ImagePath = imagePath;
+        }
+
+        public virtual int ClaculateDamage(Player player)
+        {
+            return Math.Max(1, AttackDMG - player.DefensePower);
+        }
     }
 }
